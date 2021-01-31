76f4bbd71ee4e64f22dc7c73d33a9bb1a92b8f9b
WajihaNiazi <wajihaniazi204@gmail.com>
2021-01-31 12:21:54 +0430

    first question

---
+++ src/srceens/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const Home = () => {
+  return <Text style={styles.text}>HomeScreen</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default Home;
+
