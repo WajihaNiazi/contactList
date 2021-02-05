9958c7b9ec97f7899c5472fd0da1b9dad924badb
WajihaNiazi <wajihaniazi204@gmail.com>
2021-02-05 22:54:05 +0430

    Video 8: 8-contact-list(using_database)

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useEffect} from 'react';
 import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
 import {MaterialIcons} from '@expo/vector-icons'
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import * as SQLite from "expo-sqlite";
 const Stack = createStackNavigator();
+const db = SQLite.openDatabase('contacts.db')
 import Contacts from "./screens/Contacts";
 import Profile from "./screens/profile";
 import CreateContacts from "./screens/createContacts";
 import SearchScreen from './screens/searchScreen';
 export default function App() {
+  useEffect(()=>{
+     db.transaction(tx=>{
+       tx.executeSql('create table contact(id integer primary key autoincrement, name text ,phone text,email text)')
+     },[],()=>console.log('table created successfully')); 
+  })
   return (
     <View style={styles.container}>
       <NavigationContainer>
